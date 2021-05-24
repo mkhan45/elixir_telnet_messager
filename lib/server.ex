61ee1711954408adba41912f142bb9982ff1bdc9
Mikail Khan <mikail.khan45@gmail.com>
2021-05-24 14:03:17 -0400

    reduced screen flash on update

--- lib/server.ex
+++ lib/server.ex
@@ -19,14 +19,11 @@ defmodule Server do
         clear_term(client)
         write_line("Enter Username: ", client)
         {:ok, username} = :gen_tcp.recv(client, 0)
-        clear_term(client)
-        write_line("\n> ", client)
 
         username = String.trim(username)
         Server.ClientStore.add_client(client, username)
 
-        messages = Server.MessageBuffer.get_messages() |> Enum.join()
-        write_line(messages, client)
+        refresh_screen(client)
         serve(client)
       end)
 
@@ -43,12 +40,7 @@ defmodule Server do
         message = "#{clients[socket]}: #{message}"
         Server.MessageBuffer.add_message(message)
 
-        messages = Server.MessageBuffer.get_messages() |> Enum.reverse() |> Enum.join()
-
-        for {client, _} <- clients do
-          clear_term(client)
-          write_line(messages <> "\n> ", client)
-        end
+        for {client, _} <- clients, do: refresh_screen(client)
 
         serve(socket)
 
@@ -58,6 +50,12 @@ defmodule Server do
         IO.inspect(Server.ClientStore.get_clients())
         nil
     end
+  end
+
+  defp refresh_screen(socket) do
+    messages = Server.MessageBuffer.get_messages() |> Enum.reverse() |> Enum.join()
+    out_str = "\u001B[2J" <> messages <> "\n> "
+    :gen_tcp.send(socket, out_str)
   end
 
   defp write_line(line, socket), do: :gen_tcp.send(socket, line)
